7c99172203fbe5dc7d61fb92c04cb02bd19f4f03
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-07-24 23:32:05 +0300

    Add main application and navigation components with styles and assets

---
+++ adidas/my-react-app/src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import "./NavBar.css";
+import adidasPng from "../photo/adidas.png";
+import profile from "../photo/Conta.png";
+import likes from "../photo/ListadeDesejos.png";
+import cart from "../photo/Carrinho.png";
+
+function NavBar() {
+  return (
+    <>
+      <div className="nav-text">
+        <div className="navAbove">
+          NEW COLLABS FOR THIS WEEK. CHECK OUT THE ADICLUB FOR MORE{" "}
+        </div>
+      </div>
+
+      <div className="nav-container">
+        <img src={adidasPng} alt="" />
+        <p>WOMAN</p>
+        <p>MEN</p>
+        <p>KIDS</p>
+        <p>NEWS</p>
+        <p>SPORTS</p>
+        <p>ADIDAS WORLD</p>
+        <div>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search...."
+          />
+          {/* <img src= {#} alt="search" /> */}
+        </div>
+        <div>
+          <img src={profile} alt="" />
+        </div>
+        <div>
+          <img src={likes} alt="" />
+        </div>
+        <div>
+          <img src={cart} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
