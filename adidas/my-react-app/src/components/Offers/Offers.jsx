f701bb7f0d6dc83da17ae2294092764492f3b5d1
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-07-26 23:33:20 +0300

    Add Offers component with styling for promotional offers

---
+++ adidas/my-react-app/src/components/Offers/Offers.jsx
@@ -0,0 +1,29 @@
+import "./Offers.css";
+
+function Offers() {
+  return (
+    <>
+      <div className="girlImage">
+        <p></p>
+      </div>
+      <div>
+        <p>
+          60% OFF + free shipping: only for adiclub members, Check your
+          accountand start buying for benefits on adiWeek
+        </p>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <p>BUY NOW</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>WHAT IS ADIDAS?</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Offers;
