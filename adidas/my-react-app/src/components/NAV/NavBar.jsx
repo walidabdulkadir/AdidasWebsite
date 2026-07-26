6b8f58436e18ec884dee22dfb975b7a3ec2a9f34
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-07-26 23:33:09 +0300

    Refactor NavBar component structure and styles

--- adidas/my-react-app/src/components/NAV/NavBar.jsx
+++ adidas/my-react-app/src/components/NAV/NavBar.jsx
@@ -13,31 +13,33 @@ function NavBar() {
         </div>
       </div>
 
-      <div className="nav-container">
-        <img src={adidasPng} alt="" />
-        <p>WOMAN</p>
-        <p>MEN</p>
-        <p>KIDS</p>
-        <p>NEWS</p>
-        <p>SPORTS</p>
-        <p>ADIDAS WORLD</p>
-        <div>
-          <input
-            type="search"
-            name="search"
-            id="search"
-            placeholder="Search...."
-          />
-          {/* <img src= {#} alt="search" /> */}
-        </div>
-        <div>
-          <img src={profile} alt="" />
-        </div>
-        <div>
-          <img src={likes} alt="" />
-        </div>
-        <div>
-          <img src={cart} alt="" />
+      <div className="nav">
+        <div className="nav-container">
+          <img src={adidasPng} alt="" />
+          <p>WOMAN</p>
+          <p>MEN</p>
+          <p>KIDS</p>
+          <p>NEWS</p>
+          <p>SPORTS</p>
+          <p>ADIDAS WORLD</p>
+          <div>
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Search...."
+            />
+            {/* <img src= {#} alt="search" /> */}
+          </div>
+          <div>
+            <img src={profile} alt="" />
+          </div>
+          <div>
+            <img src={likes} alt="" />
+          </div>
+          <div>
+            <img src={cart} alt="" />
+          </div>
         </div>
       </div>
     </>
