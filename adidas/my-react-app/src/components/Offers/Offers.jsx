203ad5672c45f99f9551cab82b7be993e3b896e2
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-07-27 14:57:19 +0300

    Refactor Offers component structure and update styles for improved layout and responsiveness

--- adidas/my-react-app/src/components/Offers/Offers.jsx
+++ adidas/my-react-app/src/components/Offers/Offers.jsx
@@ -3,23 +3,35 @@ import "./Offers.css";
 function Offers() {
   return (
     <>
-      <div className="girlImage">
-        <p></p>
-      </div>
-      <div>
-        <p>
-          60% OFF + free shipping: only for adiclub members, Check your
-          accountand start buying for benefits on adiWeek
-        </p>
-      </div>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <p>BUY NOW</p>
+      <div className="hero-container">
+        <div className="hero-left">
+          <div className="hero-overlay">
+            <h1>
+              OUTFITS FOR <br />
+              <span>BENEFITS</span>
+            </h1>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <p>WHAT IS ADIDAS?</p>
+
+        <div className="hero-right">
+          <p className="discount">
+            60% OFF + free shipping: <span>only for adiClub members.</span>
+          </p>
+
+          <p className="description">
+            Check your account and start buying for benefits on
+            <span> adiWeek.</span>
+          </p>
+
+          <div className="hero-buttons">
+            <button className="buy-btn">
+              <span>→</span> BUY NOW
+            </button>
+
+            <button className="info-btn">
+              <span>↓</span> WHAT IS ADIDAS?
+            </button>
+          </div>
         </div>
       </div>
     </>
