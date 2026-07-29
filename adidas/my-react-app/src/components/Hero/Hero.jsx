910f1b3ddeb56f3c3c1ddad053fb3a3b15ccb5d0
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-07-30 00:54:55 +0300

    Refactor Hero component for improved layout and responsiveness; update ShoesContainer to clean up commented code

--- adidas/my-react-app/src/components/Hero/Hero.jsx
+++ adidas/my-react-app/src/components/Hero/Hero.jsx
@@ -1,37 +1,30 @@
-import "./Hero.css";
-// import arrow from "../../assets/photo/arrow2.png";
+// import "./Hero.css";
+import cover from "../../assets/photo/Cover Image.png";
 
 function Hero() {
   return (
     <>
-      <div className="cover-img">
-        <div className="cover-overlay">
-          <h1>
+      <div
+        className="relative w-full h-[500px] bg-cover bg-center"
+        style={{ backgroundImage: `url(${cover})` }}
+      >
+        <div className="absolute left-20 top-1/2 -translate-y-1/2">
+          <h1 className="text-7xl font-bold ">
             ADICLUB <br /> IS SHOWING OFF
           </h1>
-          <p className="cover-desc">
+          <p className="tracking-[0.1rem] text-white-300">
             Exclusive releases for our members and more ways <br />
             to unlock those things that you love.
           </p>
-          <div className="hero-button">
-            <button className="buy-btn">
-              <span>→</span> SEE MORE
+          <div className=" gap-40 mt-5 font-bold">
+            <button className="flex align-items gap-3 bg-transparent border-none text-2xl text-white font-bold pointer-cursor mb-10 hover:text-[#d7cb4a]">
+              <span className="text-3xl">→</span> SEE MORE
             </button>
 
-            <button className="info-btn">
-              <span>→</span> BUY NOW
+            <button className="flex align-items gap-3 bg-transparent border-none text-2xl  text-white font-bold pointer-cursor hover:text-[#d7cb4a]">
+              <span className="text-3xl">→</span> BUY NOW
             </button>
           </div>
-          {/* <div className="cover-btns">
-            <a className="cover-btn">
-              <img src={arrow} alt="arrow" className="arrow" />
-              SEE MORE
-            </a>
-            <a className="cover-btn">
-              <img src={arrow} alt="arrow" className="arrow" />
-              BUY NOW
-            </a>
-          </div> */}
         </div>
       </div>
     </>
